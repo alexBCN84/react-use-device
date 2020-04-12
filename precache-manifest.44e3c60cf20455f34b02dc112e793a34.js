32a784d65b964200dce2a1c46af09038fe1f64dd
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2020-04-12 20:00:57 +0200

    Updates

---
+++ precache-manifest.44e3c60cf20455f34b02dc112e793a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c52e75f3464325b5567f2b47b5fc31",
+    "url": "/react-use-device/index.html"
+  },
+  {
+    "revision": "2fe9733ea743409aadb1",
+    "url": "/react-use-device/static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "b168e7cf6047880a644f",
+    "url": "/react-use-device/static/js/2.7c341f94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-use-device/static/js/2.7c341f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe9733ea743409aadb1",
+    "url": "/react-use-device/static/js/main.ea83c2cc.chunk.js"
+  },
+  {
+    "revision": "95eef84d58d52f9446a9",
+    "url": "/react-use-device/static/js/runtime-main.4762f558.js"
+  }
+]);
